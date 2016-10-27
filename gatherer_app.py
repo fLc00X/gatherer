da3df67ba0cbebf262c2a88f8190a9890474aff5
fLc00X <valeriy.arsenev@gmail.com>
2016-10-27 14:59:59 -0400

    Create gatherer_app.py

---
+++ gatherer_app.py
@@ -0,0 +1,14 @@
+import flask
+
+api_version = '0.1'
+api_url_prefix = '/api/gatherer/v1'
+
+app = flask.Flask(__name__)
+
+@app.route(api_url_prefix + '/', methods = ['GET'])
+def content():
+    return flask.jsonify({'name': 'gatherer API',
+                          'version': api_version})
+if __name__ == '__main__':
+    print 'gatherer API (' + api_version + ')'
+    app.run(debug = False)
