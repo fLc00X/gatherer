128f96dad399ec465fb1b5c809cb523c89cba939
fLc00X <valeriy.arsenev@gmail.com>
2016-10-28 00:17:36 -0400

    Update gatherer_app.py

--- gatherer_app.py
+++ gatherer_app.py
@@ -5,7 +5,8 @@ api_url_prefix = '/api/gatherer/v1'
 
 app = flask.Flask(__name__)
 
-@app.route(api_url_prefix + '/', methods = ['GET'])
+#@app.route(api_url_prefix + '/', methods = ['GET'])
+@app.route('/', methods = ['GET'])
 def content():
     return flask.jsonify({'name': 'gatherer API',
                           'version': api_version})
