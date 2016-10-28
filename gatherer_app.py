7db2ee007ca226dccabd0d0d942891dea6f15643
fLc00X <valeriy.arsenev@gmail.com>
2016-10-28 12:16:05 -0400

    Update gatherer_app.py

--- gatherer_app.py
+++ gatherer_app.py
@@ -1,4 +1,52 @@
 import flask
+import json
+import time
+import urllib
+import urllib2
+
+def readUrl(url):
+    request = urllib2.Request(url)
+    try:
+        response = urllib2.urlopen(request)
+        return (response.getcode(), response.read())
+    except urllib2.HTTPError as httpError:
+        return (httpError.getcode(), httpError.read())
+    except Exception as error:
+        return (-1, "readUrl::error occurred:" + str(error))
+
+def gatherStation(host, station, name):
+    # read & publish
+    print 'station:' + station + ' (' + name + ')'
+    print 'http://' + host + '/' + station
+    code, data = readUrl('http://' + host + '/' + station)
+    if code == 200:
+        prices = []
+        for line in data.split('\n'):
+            if 'price-display credit-price' in line:
+                prices.append(line.split('>')[1].split('<')[0])
+        #publish('/gasstations/' + station + '/latest',
+        #        {'timestamp': time.strftime('%Y-%m-%d %H:%M:%S',
+        #                                    time.localtime()),
+        #         'regular':  prices[0] if len(prices) > 0 else -1.00,
+        #         'midgrade': prices[1] if len(prices) > 1 else -1.00,
+        #         'premium':  prices[2] if len(prices) > 2 else -1.00})
+        data = {'timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+                'regular':  prices[0] if len(prices) > 0 else -1.00,
+                'midgrade': prices[1] if len(prices) > 1 else -1.00,
+                'premium':  prices[2] if len(prices) > 2 else -1.00}
+        print data
+        return data
+    else:
+        print "error occurred"
+
+    # verify
+    #code, data = readUrl(apiUri + \
+    #                     '/gasstations/' + station + '/latest' + \
+    #                     '?' + \
+    #                     urllib.urlencode({'api_key': apiGetKey}))
+    #if code == 200:
+    #    data = json.loads(data)
+    #print 'code:' + str(code) + ',data:' + str(data)
 
 app_version = '0.1'
 
@@ -9,11 +57,18 @@ def content():
     return flask.jsonify({'name': 'gatherer',
                           'version': app_version})
 
-@app.route('/gatherer_app', methods = ['GET'])
+@app.route('/text', methods = ['GET'])
 def text():
     r = flask.make_response('gatherer_app')
     r.mimetype = 'text/plain'
     return r
+
+@app.route('/gather_stations', methods = ['GET'])
+def gather_stations():
+    print 'gatherer_worker started'
+    host = 'www.gasbuddy.com/Station'
+    stations = {'2731': 'Station@Someplace'}
+    return flask.jsonify([gatherStation(host, station, name) for station, name in stations.items()])
 
 if __name__ == '__main__':
     print 'gatherer (' + app_version + ')'
