d9f4dbd67f01201baffe37e807edad9543cbc0df
fLc00X <valeriy.arsenev@gmail.com>
2016-11-03 23:07:08 -0400

    Update gatherer_worker.py

--- gatherer_worker.py
+++ gatherer_worker.py
@@ -29,15 +29,18 @@ def gatherStation(host, station, name):
     if code == 200:
         prices = []
         for line in data.split('\n'):
-            if 'price-display credit-price' in line:
-                prices.append(line.split('>')[1].split('<')[0])
+            if 'price-display' in line:
+                if ' credit-price' in line
+                    prices.append(line.split('>')[1].split('<')[0])
+                else:
+                    prices.append('-1.00')
         return {'station': station,
                 'name': name,
                 'status': 'ok',
                 'timestamp': timestamp(),
-                'regular': prices[0] if len(prices) > 0 else -1.00,
-                'midgrade': prices[1] if len(prices) > 1 else -1.00,
-                'premium':  prices[2] if len(prices) > 2 else -1.00}
+                'regular': prices[1] if len(prices) > 0 else -1.00,
+                'midgrade': prices[3] if len(prices) > 1 else -1.00,
+                'premium':  prices[5] if len(prices) > 2 else -1.00}
     else:
         return {'station': station,
                 'name': name,
