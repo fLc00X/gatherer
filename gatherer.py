2869d9f7741c923ab84759bf4248851e35adb148
fLc00X <valeriy.arsenev@gmail.com>
2016-11-09 19:30:38 -0500

    Update gatherer.py

--- gatherer.py
+++ gatherer.py
@@ -1,55 +1,13 @@
 #!/bin/python
 
-import datetime
 import os
-import time
 import threading
-import urllib
-import urllib2
 
 import rxtxapi
+from gasoline_gatherer import GasolineGatherer
 
 def log(message):
     print 'gatherer|' + message
-
-def readUrl(url):
-    request = urllib2.Request(url)
-    try:
-        response = urllib2.urlopen(request)
-        return (response.getcode(), response.read())
-    except urllib2.HTTPError as httpError:
-        return (httpError.getcode(), httpError.read())
-    except Exception as error:
-        return (-1, "readUrl::error occurred:" + str(error))
-
-def timestamp():
-    return datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
-
-def gatherStation(url, station, name):
-    code, data = readUrl(url + '/' + station)
-    if code == 200:
-        prices = []
-        for line in data.split('\n'):
-            if '<div class="price-display' in line:
-                if 'credit-price' in line:
-                    prices.append(line.split('>')[1].split('<')[0])
-                else:
-                    prices.append(-1.00)
-        return {'station': station,
-                'name': name,
-                'status': 'ok',
-                'timestamp': timestamp(),
-                'regular': prices[1] if len(prices) > 0 else -1.00,
-                'midgrade': prices[3] if len(prices) > 1 else -1.00,
-                'premium':  prices[5] if len(prices) > 2 else -1.00}
-    else:
-        return {'station': station,
-                'name': name,
-                'status': 'error',
-                'timestamp': timestamp(),
-                'regular': -1.00,
-                'midgrade': -1.00,
-                'premium': -1.00}
 
 def getVar(name):
     if os.environ.get(name):
@@ -57,31 +15,21 @@ def getVar(name):
     else:
         raise Exception(name + ' is not set up')
 
-def getStations():
-    return [tuple(s.split(':')) for s in getVar('GATHERER_WORKER_GAS_STATIONS_STATIONS').split(',')]
-
-def getUrl():
-    return getVar('GATHERER_WORKER_GAS_STATIONS_URL')
-
-def getInterval():
-    return int(getVar('GATHERER_WORKER_GAS_STATIONS_INTERVAL'))
-
-def getRxtxApi():
-    return rxtxapi.RxtxApi(getVar('RXTXAPI_URI'),
-                           {'POST': getVar('RXTXAPI_POST_KEY'),
-                            'PUT': getVar('RXTXAPI_PUT_KEY'),
-                            'GET': getVar('RXTXAPI_GET_KEY'),
-                            'DELETE': getVar('RXTXAPI_DELETE_KEY')})
-
 def worker():
+    api = rxtxapi.RxtxApi(getVar('RXTXAPI_URI'),
+                          {'POST': getVar('RXTXAPI_POST_KEY'),
+                           'PUT': getVar('RXTXAPI_PUT_KEY'),
+                           'GET': getVar('RXTXAPI_GET_KEY'),
+                           'DELETE': getVar('RXTXAPI_DELETE_KEY')})
+    gasolineGatherer = GasolineGatherer(int(getVar('GATHERER_WORKER_GAS_STATIONS_INTERVAL')),
+                                        api,
+                                        getVar('GATHERER_WORKER_GAS_STATIONS_URL'),
+                                        [tuple(s.split(':')) for s in getVar('GATHERER_WORKER_GAS_STATIONS_STATIONS').split(',')]
     while True:
-        api = getRxtxApi()
-        for d in [gatherStation(getUrl(), station, name) for station, name in getStations()]:
-            log('publish:' + str(d))
-            api.publish('gas_stations/' + d['station'], d)
-        time.sleep(getInterval())
+        log('gasolineGatherer:' + str(gasolineGatherer.gather()))
+        time.sleep(int(getVar('GATHERER_WORKER_INTERVAL')))
 
-log('version 0.7')
+log('version 0.8')
 log(__name__)
 if __name__ == '__main__':
     worker()
