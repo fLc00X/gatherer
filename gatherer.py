ce4d1030e0216bc5bf8ceba833ce783775d4144d
fLc00X <valeriy.arsenev@gmail.com>
2016-11-09 21:33:50 -0500

    Update gatherer.py

--- gatherer.py
+++ gatherer.py
@@ -16,7 +16,7 @@ def env(name):
     else:
         raise Exception(name + ' is not set up')
 
-def worker():
+def gather():
     gasolineGatherer = GasolineGatherer(int(env('GATHERER_WORKER_GAS_STATIONS_INTERVAL')),
                                         RxtxApi(env('RXTXAPI_URI'),
                                                 {'POST': env('RXTXAPI_POST_KEY'),
@@ -26,18 +26,18 @@ def worker():
                                         env('GATHERER_WORKER_GAS_STATIONS_URL'),
                                         [s.split(':') for s in env('GATHERER_WORKER_GAS_STATIONS_STATIONS').split(',')])
     while True:
-        log('start gathering ...')
+        log('gathering ...')
         data = gasolineGatherer.gather()
         if data:
             log('gasoline:' + str(data))
-        log('done gathering')
+        log('finished')
         time.sleep(int(env('GATHERER_WORKER_INTERVAL')))
 
-log('version 0.10')
+log('version 0.11')
 log(__name__)
 if __name__ == '__main__':
-    worker()
+    gather()
 else:
-    t = threading.Thread(target = worker)
+    t = threading.Thread(target = gather)
     t.start()
 log('done')
