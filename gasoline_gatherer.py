5f3ddc7a609f37e8d21b42b77802b68be45f24ba
fLc00X <valeriy.arsenev@gmail.com>
2018-06-22 19:29:08 -0400

    Accommodate change in the line with price

--- gasoline_gatherer.py
+++ gasoline_gatherer.py
@@ -13,21 +13,20 @@ class GasolineGatherer(base_gatherer.BaseGatherer):
         result = {'station': station,
                   'name': name,
                   'status': 'error',
-                  'timestamp': self.timestamp(),
-                  'regular': -1.00,
-                  'midgrade': -1.00,
-                  'premium': -1.00}
+                  'timestamp': self.timestamp()}
+        grades = ['regular', 'midgrade', 'premium']
+        for grade in grades:
+            result[grade] = -1.0
         code, data = readUrl(url = self.url + '/' + station,
                              headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'})
         if code == 200:
-            prices = []
+            index = 0
             for line in data.split('<'):
-                if 'ui header styles__price___1wJ_R' in line and '$' in line:
-                    prices.append(line.split('$')[1])
+                if 'styles__price___1wJ_R">' in line:
+                    if '$' in line and index < len(grades):
+                        result[grades[index]] = line.split('$')[1]
+                    index += 1
             result['status'] = 'ok'
-            result['regular'] = prices[0] if len(prices) >= 1 else -1.00
-            result['midgrade'] = prices[1] if len(prices) >= 2 else -1.00
-            result['premium'] = prices[2] if len(prices) >= 3 else -1.00
         return result
 
     def collect(self):
